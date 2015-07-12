94bde6d1fa6b0c5eaed44a82e913438790d2a901
Zoe <zadelmanz@gmail.com>
2015-07-12 09:09:52 -0600

    added code to 99 bottles question

---
+++ ninetynine.js
@@ -0,0 +1,3 @@
+for (var number=99; number>0; number--) {
+  console.log(number + " bottles of beer on the wall,\n" + number + " bottles of beer,\nYou take one down and pass it around,\n" + number + " bottles of beer on the wall!\n" )
+}
