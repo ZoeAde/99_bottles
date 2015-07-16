c60717fee16b5e971bb9986bfcd46d6a4558bee0
ZoeAde <zoe.a.adelman@gmail.com>
2015-07-16 08:01:26 -0600

    fixing error copy at end of if statement

--- ninetynine.js
+++ ninetynine.js
@@ -1,5 +1,4 @@
 for (var number=99; number>0; number--) {
-<<<<<<< HEAD
   if (number === 1) {
     console.log(number + " bottle of beer on the wall,\n" + number + " bottle of beer,\nYou take one down and pass it around,\n" + number + " bottle of beer on the wall!\n" )
   }
@@ -7,7 +6,3 @@ for (var number=99; number>0; number--) {
   console.log(number + " bottles of beer on the wall,\n" + number + " bottles of beer,\nYou take one down and pass it around,\n" + number + " bottles of beer on the wall!\n" )
 }
 }
-=======
-  console.log(number + " bottles of beer on the wall,\n" + number + " bottles of beer,\nYou take one down and pass it around,\n" + number + " bottles of beer on the wall!\n" )
-}
->>>>>>> 8e5c0a19d4e1eb3eddd125feb72c1e4b30d60ad6
