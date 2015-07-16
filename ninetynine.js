4d347c245dea43e6f29fee63e8f6aab0c0294f40
ZoeAde <zoe.a.adelman@gmail.com>
2015-07-15 21:17:48 -0600

    changes one bottle of beer to singular

--- ninetynine.js
+++ ninetynine.js
@@ -1,3 +1,8 @@
 for (var number=99; number>0; number--) {
+  if (number === 1) {
+    console.log(number + " bottle of beer on the wall,\n" + number + " bottle of beer,\nYou take one down and pass it around,\n" + number + " bottle of beer on the wall!\n" )
+  }
+  else {
   console.log(number + " bottles of beer on the wall,\n" + number + " bottles of beer,\nYou take one down and pass it around,\n" + number + " bottles of beer on the wall!\n" )
+}
 }
